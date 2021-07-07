9fbb637f98e4b317f97d23b65a6ee4f7cce5e853
JP Given <jgiven@gmail.com>
2021-07-07 13:13:16 -0400

    ActionController::Parameters no longer inherits from Hash, in an attempt to discourage people from using Hash-related methods on the request parameters without explicitly filtering them.

--- lib/rubycas-client-rails.rb
+++ lib/rubycas-client-rails.rb
@@ -377,7 +377,7 @@ module RubyCAS
         
         params = controller.params.dup
         params.delete(:ticket)
-        service_url = controller.url_for(params)
+        service_url = controller.url_for(params.to_enum.to_h)
         log.debug("Guessed service url: #{service_url.inspect}")
         return service_url
       end
